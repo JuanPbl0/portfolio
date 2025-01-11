2084954bc4819ec6a118b45e42535fd723ac7193
Juan Pablo <jp.bajuan@gmail.com>
2025-01-11 11:34:21 -0300

    Melhorias na responsividade e ajustes no layout

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function baixarCurriculo() {
     link.download = 'curriculoJuan.pdf';
     link.click();
 }
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.style.background = window.scrollY > 50 ? 'rgba(0, 0, 0, 0.9)' : 'rgba(0, 0, 0, 0.7)';
+});
+
